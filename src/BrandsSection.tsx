441922c778d8134338214a49088fb0d98a9c1c57
Zalim Tsorionov <ztsorionov@oneroyal.com>
2025-06-24 12:33:37 +0300

    24JUN25

--- src/BrandsSection.tsx
+++ src/BrandsSection.tsx
@@ -15,8 +15,8 @@ const BrandsSection = () => {
 
   return (
     <section className="brands pv">
-      <h2 className="t-38 up">Our Brands</h2>
-      <p className="t-18">From VIP journeys to unforgettable events and powerful business forums, our brands redefine excellence. Personal Travel Assist crafts tailor-made executive travel experiences with a touch of luxury. Memory Makers Events turns high-level corporate visions into bold, immersive realities. HR Summit Cy unites top HR minds through exclusive summits that spark innovation and leadership.</p>
+      <h2 className="t-38 up section-title">Our Brands</h2>
+      <p className="t-18 section-description">From VIP journeys to unforgettable events and powerful business forums, our brands redefine excellence. Personal Travel Assist crafts tailor-made executive travel experiences with a touch of luxury. Memory Makers Events turns high-level corporate visions into bold, immersive realities. HR Summit Cy unites top HR minds through exclusive summits that spark innovation and leadership.</p>
       <div className="brands-tabs">
         {tabNames.map((name, idx) => (
           <button
@@ -35,20 +35,20 @@ const BrandsSection = () => {
             <div className="brand-logo">
               <img src={ptaLogo} alt="Personal Travel Assist Logo" />
             </div>
-            <h3>Personal Travel Assist</h3>
+            <h3 className='card-brand-title'>Personal Travel Assist</h3>
             <div className="brand-section">
               <div className="brand-section-title">
-                <span className="brand-title-bold">A</span><span className="brand-title-gradient">BOUT THE BRAND</span>
+                <div className="t-20 card-title">ABOUT THE BRAND</div>
               </div>
-              <p className="t-18">
+              <p className="t-18 card-description">
                 <strong>Personal Travel Assist</strong> is an international company providing services in the field of <strong>booking hotels, air tickets, transfers, excursions</strong>, as well as creating <strong>individual routes at the client's request</strong> (logistics, flight plans, etc.).
               </p>
             </div>
             <div className="brand-section">
               <div className="brand-section-title">
-                <span className="brand-title-bold">S</span><span className="brand-title-gradient">ERVICES</span>
+                <div className="t-20 card-title">SERVICES</div>
               </div>
-              <p className="t-18">
+              <p className="t-18 card-description">
                 We provide services to <strong>both individuals and legal entities</strong> in the following areas: <strong>excursion and combined tours, therapeutic and wellness programs, training for children and adults</strong>, work with corporate clients and businesses, expeditions, event tourism, weddings, etc.<br/><br/>
                 Our company professionally resolves issues of <strong>corporate travel</strong> for training, workshops, business conferences, exhibitions, recreation or active pastime of the client company's team for <strong>more than 15 years.</strong>
               </p>
